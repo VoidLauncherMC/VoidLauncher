4245bd7601b1d42479736bb69ba0e725c5e30b6e
dabdob-craft <dabdobcraft2@gmail.com>
2026-01-11 13:57:22 +0100

    Implement Microsoft token retrieval functionality

--- app/src/main/java/com/voidlauncher/MicrosoftAuth.java
+++ app/src/main/java/com/voidlauncher/MicrosoftAuth.java
@@ -1,15 +1,36 @@
 package com.voidlauncher;
 
+import okhttp3.*;
+import java.io.IOException;
+
 public class MicrosoftAuth {
     public static final String CLIENT_ID = "00000000402b5328";
     public static final String SCOPE = "service::user.auth.xboxlive.com::MBI_SSL";
     public static final String REDIRECT_URL = "https://login.live.com/oauth20_desktop.srf";
-    
+
+    private final OkHttpClient client = new OkHttpClient();
+
     public static String getLoginUrl() {
         return "https://login.live.com/oauth20_authorize.srf" +
                 "?client_id=" + CLIENT_ID +
                 "&response_type=code" +
-                "&scope=" + SCOPE +
+                "&scope=service::user.auth.xboxlive.com::MBI_SSL" +
                 "&redirect_uri=" + REDIRECT_URL;
+    }
+
+    public void getMicrosoftToken(String code, Callback callback) {
+        RequestBody formBody = new FormBody.Builder()
+                .add("client_id", CLIENT_ID)
+                .add("code", code)
+                .add("grant_type", "authorization_code")
+                .add("redirect_uri", REDIRECT_URL)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://login.live.com/oauth20_token.srf")
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(callback);
     }
 }
