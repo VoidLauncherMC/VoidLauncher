0c5ce22c48101a290040bb04f12020635c6ef377
dabdob-craft <dabdobcraft2@gmail.com>
2026-01-14 12:25:55 +0100

    Refactor GameDownloader to accept Context
    Refactored GameDownloader to use Context and updated downloadFile method signature.

--- app/src/main/java/com/voidlauncher/GameDownloader.java
+++ app/src/main/java/com/voidlauncher/GameDownloader.java
@@ -1,44 +1,14 @@
 package com.voidlauncher;
 
-import okhttp3.*;
-import java.io.*;
+import android.content.Context;
 
 public class GameDownloader {
-    private final OkHttpClient client = new OkHttpClient();
+    private Context context;
 
-    public void downloadFile(String url, File outputFile, DownloadCallback callback) {
-        Request request = new Request.Builder().url(url).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                callback.onError(e.getMessage());
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    callback.onError("Server error");
-                    return;
-                }
-
-                try (InputStream is = response.body().byteStream();
-                     FileOutputStream fos = new FileOutputStream(outputFile)) {
-                    
-                    byte[] buffer = new byte[8192];
-                    int read;
-                    while ((read = is.read(buffer)) != -1) {
-                        fos.write(buffer, 0, read);
-                    }
-                    callback.onSuccess();
-                } catch (Exception e) {
-                    callback.onError(e.getMessage());
-                }
-            }
-        });
+    public GameDownloader(Context context) {
+        this.context = context;
     }
 
-    public interface DownloadCallback {
-        void onSuccess();
-        void onError(String error);
+    public void downloadFile(String url, String outputPath) {
     }
 }
