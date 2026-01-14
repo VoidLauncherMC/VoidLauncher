844c31ff369a7c5885c14155e0ee15f68302f6a5
dabdob-craft <dabdobcraft2@gmail.com>
2026-01-14 12:21:34 +0100

    Add AuthManager.java for authentication management

---
+++ app/src/main/java/com/voidlauncher/AuthManager.java
@@ -0,0 +1,17 @@
+package com.voidlauncher;
+
+import android.content.Context;
+
+public class AuthManager {
+    private Context context;
+
+    public AuthManager(Context context) {
+        this.context = context;
+    }
+
+    public void loginMicrosoft(String code) {
+    }
+
+    public void loginYggdrasil(String username, String password) {
+    }
+}
