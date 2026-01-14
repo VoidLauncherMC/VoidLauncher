88dea6486affd4b0ab6eb60d4de9779543a97678
dabdob-craft <dabdobcraft2@gmail.com>
2026-01-14 14:23:59 +0100

    Create LoginType.java

---
+++ app/src/main/java/com/voidlauncher/LoginType.java
@@ -0,0 +1,11 @@
+package com.voidlauncher;
+
+public class LoginType {
+    public String name;
+    public int iconRes;
+
+    public LoginType(String name, int iconRes) {
+        this.name = name;
+        this.iconRes = iconRes;
+    }
+}
