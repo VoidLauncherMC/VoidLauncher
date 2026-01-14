0ab94eff72c5dce43ed681cd63ee8f3812a55964
dabdob-craft <dabdobcraft2@gmail.com>
2026-01-14 12:24:23 +0100

    Refactor VersionManager to remove network calls
    Removed OkHttpClient usage and modified fetchVersions method to return a List of versions.

--- app/src/main/java/com/voidlauncher/VersionManager.java
+++ app/src/main/java/com/voidlauncher/VersionManager.java
@@ -1,19 +1,11 @@
 package com.voidlauncher;
 
-import okhttp3.*;
-import java.io.IOException;
+import java.util.List;
 
 public class VersionManager {
-    private static final String MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
-    private final OkHttpClient client = new OkHttpClient();
+    public VersionManager() {}
 
-    public void fetchVersions(Callback callback) {
-        Request request = new Request.Builder().url(MANIFEST_URL).build();
-        client.newCall(request).enqueue(callback);
-    }
-    
-    public void fetchVersionDetails(String url, Callback callback) {
-        Request request = new Request.Builder().url(url).build();
-        client.newCall(request).enqueue(callback);
+    public List<String> fetchVersions() {
+        return null;
     }
 }
